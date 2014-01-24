559feb80dd310f8ecd89b5ce73b9d4abcf0ebcff
Rischan in PC Lab <rischanlab@gmail.com>
2014-01-24 22:27:27 +0900

    Fix code beta

--- gen/rischanlab/github/io/wifiho/R.java
+++ gen/rischanlab/github/io/wifiho/R.java
@@ -26,15 +26,15 @@ public final class R {
         public static final int ic_refresh=0x7f020002;
     }
     public static final class id {
-        public static final int LayoutHeader=0x7f080008;
+        public static final int LayoutHeader=0x7f080006;
         public static final int StackMain=0x7f080005;
         public static final int action_settings=0x7f08000c;
         public static final int button=0x7f08000b;
         public static final int buttonRefresh=0x7f080009;
         public static final int groupTabs=0x7f080002;
         public static final int linearBottom=0x7f08000a;
-        public static final int listAP=0x7f080006;
-        public static final int listRegAP=0x7f080007;
+        public static final int listAP=0x7f080007;
+        public static final int listRegAP=0x7f080008;
         public static final int tabAvailable=0x7f080003;
         public static final int tabRegistered=0x7f080004;
         public static final int text=0x7f080000;
@@ -53,9 +53,11 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int connectiontoString=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int strJudul=0x7f05000b;
         public static final int strListRegAP=0x7f050007;
         public static final int strWifidisable=0x7f050008;
         public static final int stravailable=0x7f050005;
+        public static final int strconn=0x7f05000a;
         public static final int strregistered=0x7f050006;
         public static final int wifi_is_disabled=0x7f050009;
     }
