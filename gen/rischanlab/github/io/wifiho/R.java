547e2c3f4f43f59e5e16bc09b66a76905009ec3f
Rischan in PC Lab <rischanlab@gmail.com>
2014-01-24 19:55:20 +0900

    Add second layout for registered AP list

--- gen/rischanlab/github/io/wifiho/R.java
+++ gen/rischanlab/github/io/wifiho/R.java
@@ -21,21 +21,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int delete_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int StackMain=0x7f080005;
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
+        public static final int button=0x7f080008;
         public static final int groupTabs=0x7f080002;
         public static final int listAP=0x7f080006;
         public static final int listRegAP=0x7f080007;
         public static final int tabAvailable=0x7f080003;
         public static final int tabRegistered=0x7f080004;
+        public static final int text=0x7f080000;
         public static final int textStatus=0x7f080001;
-        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_ho=0x7f030000;
+        public static final int row=0x7f030001;
     }
     public static final class menu {
         public static final int ho=0x7f070000;
@@ -46,6 +49,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int connectiontoString=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int strListRegAP=0x7f050007;
         public static final int stravailable=0x7f050005;
         public static final int strregistered=0x7f050006;
     }
